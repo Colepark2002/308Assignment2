118efbf58cb4d25b9711e23e360be76bb8f0c834
Jacob Shapero <jwshapero@gmail.com>
2022-10-23 13:57:26 -0700

    Add files via upload

---
+++ Top Secret Government Files/308Assignment2-master/Assignment 2/src/Circle.java
@@ -0,0 +1,28 @@
+import java.awt.*;
+
+public class Circle {
+    int x, y;
+    Color color;
+
+    public Circle(int x, int y, Color color){
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+}
